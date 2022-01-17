5eb4538dd4a1c2cdd2e2ee3c975e4db058e49686
Muhammet Gumus <muhammetgms95@gmail.com>
2022-01-17 23:02:06 +0300

    eventfindercontroller test classes added

---
+++ src/test/java/com/event/finder/EventFinderControllerTest.java
@@ -0,0 +1,20 @@
+package com.event.finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@WebMvcTest
+@EnableWebMvc
+@AutoConfigureMockMvc
+public class EventFinderControllerTest {
+
+
+    @Test
+    public void findEventByIdTest(){
+
+    }
+
+
+}
