41d1c50db12f95cb10813b5ac46244b0f6c6c91e
Muhammet Gumus <muhammetgms95@gmail.com>
2022-01-17 23:31:30 +0300

    findEventByIdTest test is added

--- src/test/java/com/event/finder/EventFinderControllerTest.java
+++ src/test/java/com/event/finder/EventFinderControllerTest.java
@@ -1,18 +1,50 @@
 package com.event.finder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(value = EventFinderController.class)
 @EnableWebMvc
 @AutoConfigureMockMvc
 public class EventFinderControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
 
     @Test
-    public void findEventByIdTest(){
+    public void findEventByIdTest() {
+        Event event = new Event();
+        String expectedValue = objectMapper.writeValueAsString(event);
+        Mockito.when(eventFinderService.findByEventId(Mockito.anyString())).thenReturn(event);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/findById/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedValue));
+
 
     }
 
